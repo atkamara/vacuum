705d1bbb6fbfa1767706382602e4ecec12ad98ed
atkamara <abdourahmane29@outlook.com>
2024-04-27 17:02:40 +0000

    xpaths definition

--- src/vacuum/Fields.py
+++ src/vacuum/Fields.py
@@ -1,15 +1,23 @@
 from .Model import Field
 from .Item import MainItem
+from itertools import product
 
 @MainItem.register
+class ProductTitle(Field):
+    title = '{}[class=contains("title")][1]'.format
+    header_tags = ['h2','h3','h4','h5',title('p'),title('div')]
+    child = ['a[1]','']
+    data = ['@title','::text()']
+    xpaths = ['/'.join(p for p in case if p) for case in product(header_tags,child,data)]
+@MainItem.register
 class PublishDate(Field):
-    xpaths:list[str]
-    regex_list:list[str]
+    xpaths = [
+        'div[class="listing-card__header__date"]/text()',
+        'span/@data-bs-content',
+        'div[class="card-title ad__card-timesince"]/span/text()'
+    ]
 @MainItem.register
 class Contact(Field):
-    ...
-@MainItem.register
-class ProductTitle(Field):
     ...
 @MainItem.register
 class PublishLink(Field):
--- src/vacuum/Page.py
+++ src/vacuum/Page.py
@@ -2,5 +2,15 @@ from .Model import Page
 from .Item import MainItem
 
 class MainPage(Page):
+    xpath0 = "article"
+    div = "div[@class={}]".format
+    keywords = ["list(ing)?","item"]
+    case0 = "[\s\-]".join(keywords) 
+    case1 = "[\s\-]".join(reversed(keywords))
+    cases = [case0,case1]
+    regex = "|".join(cases)
+    xpath1 = div(f'matches("{regex}")')
+    xpath2 = div("media panel panel-default")
+    xpaths = [xpath0,xpath1,xpath2]
     def as_item(self,html):
         return MainItem(html)
