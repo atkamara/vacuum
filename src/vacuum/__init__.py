34038576cdfeabbf9211e2d36ab1c7bb489162f6
atkamara <abdourahmane29@outlook.com>
2024-04-28 17:47:17 +0000

    import priority to fields

--- src/vacuum/__init__.py
+++ src/vacuum/__init__.py
@@ -16,8 +16,8 @@ To use the package, import the specific modules as needed. For example:
     from vacuum.Page import Page
 """
 from __future__ import annotations
+from . import Fields #must be imported in priority before item
 from . import Item
-from . import Fields
 from . import Model
 from . import Page
 
