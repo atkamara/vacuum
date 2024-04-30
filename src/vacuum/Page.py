faadb7b8deb2513136a325d80bf40c51bfc7adbd
atkamara <abdourahmane29@outlook.com>
2024-04-30 09:34:00 +0000

    next page xpath strategy

--- src/vacuum/Page.py
+++ src/vacuum/Page.py
@@ -2,15 +2,15 @@ from .Model import Page
 from .Item import MainItem
 
 class MainPage(Page):
-    xpath0 = '//article[contains(@class,"item")]'
+    xpath0 = '//article[contains(@class,"item") and not(contains(@class,"cart"))]'
     div = "//div[{}]".format
-    xpath1 = div(f'contains(@class,"list") and contains(@class,"item")')
+    xpath1 = div(f'contains(@class,"list") and contains(@class,"item") ')
     xpath2 = div('@class="media panel panel-default"')
     xpaths = [xpath0,xpath1,xpath2]
+    next_test = 'contains(@class,"next") or contains(@rel,"next")'
     next_xpaths = [
-        '//a[class="prevnext"]',
-        '//a[class="page-link" and rel="next"]',
-        '//li[class="pagi-next"]/a[1]',
-        '//li.pagination-indicator/a/span[class="next"]/..' ]
+        f'//a[{next_test}]/@href',
+        '//*[contains(@class,"next")]/a[1]/@href'
+        ]
     def as_item(self,html):
-        return MainItem.relative_parse(html)
+        return MainItem.parse(html,method='relative_value')
